2095365bd33c46b0d232d264f0493f8e239224d3
Elle Abalos <elle.abalos@outlook.com>
2022-04-05 20:35:39 +0800

    updated for bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import * as bootstrap from 'bootstrap'
+import 'stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
