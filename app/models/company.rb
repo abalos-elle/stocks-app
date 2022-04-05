fdaca688788950ecb4dbe3b21f0b6121ab0f1774
ermzkie <ermawindelynlagustan@gmail.com>
2022-04-05 23:55:33 +0800

    Created new helper and functions for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,20 @@ class Company < ApplicationRecord
     validates :cik, presence: true
     validates :name, presence: true
     validates :ticker, presence: true, uniqueness: true
+
+    def get_change
+        self.latest_price-self.previous_close
+    end
+
+    def get_change_percent
+        get_change/self.previous_close*100
+    end
+
+    def get_top_5
+        Company.order(get_change).limit(5)
+    end
+
+    def self.get_bottom_5
+        Company.order("latest_price - previous_close")
+    end
 end
