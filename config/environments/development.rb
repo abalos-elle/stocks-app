d9e3570508f1c2f84570f326f7fce11d2f482108
Elle Abalos <elle.abalos@outlook.com>
2022-04-05 17:30:28 +0800

    update development config settings for letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Set default URL options for Devise mailer in each environment
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Letter_opener configuration
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
