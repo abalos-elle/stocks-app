ac4b1d782f76d61dee5a32884f8be50c618887b9
ermzkie <ermawindelynlagustan@gmail.com>
2022-04-06 22:25:11 +0800

    Updated the home controller for searching thru ransack

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @companies = Company.where("latest_price is not null")
+    # @companies = Company.where("latest_price is not null")
+    @q = Company.select([:name, :ticker, :latest_price, :previous_close]).ransack(params[:q])
+    @companies = @q.result(distinct: true)
   end
 end
