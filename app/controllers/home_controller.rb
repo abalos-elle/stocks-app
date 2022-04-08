57f5121409ea9f0d56480cb6b046a6272578e1a5
Elle Abalos <elle.abalos@outlook.com>
2022-04-08 21:31:46 +0800

    updated navbar and header

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     if !user_signed_in?
       redirect_to new_user_session_path
     else
+      @user = current_user
       @companies = Company.where("latest_price is not null")
     end
   end
