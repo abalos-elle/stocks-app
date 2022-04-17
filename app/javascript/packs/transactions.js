b7d662c9cc9e12876ee1a851b9d000ef62219e1f
ermzkie <ermawindelynlagustan@gmail.com>
2022-04-17 21:07:41 +0800

    Removed not working converted code from jquery for fixed column header of table

--- app/javascript/packs/transactions.js
+++ app/javascript/packs/transactions.js
@@ -1,21 +1,22 @@
 document.addEventListener('turbolinks:load', () => {
-    var table =  document.querySelector('table.scroll');
-    var bodyCells = document.querySelector('tbody tr:first-child').children;
-    var colWidth;
+    // var table =  document.querySelector('table.scroll');
+    // var bodyCells = document.querySelector('tbody tr:first-child').children;
+    // var colWidth;
 
-    function resize(){
-        // Get the tbody columns width array
-        colWidth = Array.from(bodyCells).map(function(element) {
-            return element.width;            
-        }).get();
+    // function resize(){
+    //     console.log(Array.from(bodyCells)[0].width);
+    //     // // Get the tbody columns width array
+    //     // colWidth = Array.from(bodyCells).map(function(element) {
+    //     //     return element.width;            
+    //     // });
         
-        // Set the width of thead columns
-        table.querySelector('thead tr').children().each(function(i, v) {
-            v.width(colWidth[i]);
-        });    
-    }
+    //     // Set the width of thead columns
+    //     table.querySelector('thead tr').children().each(function(i, v) {
+    //         v.width(colWidth[i]);
+    //     });    
+    // }
 
-    // Adjust the width of thead cells when window resizes
-    window.addEventListener('resize', resize, true);
-    resize();
+    // // Adjust the width of thead cells when window resizes
+    // window.addEventListener('resize', resize, true);
+    // resize();
 })
