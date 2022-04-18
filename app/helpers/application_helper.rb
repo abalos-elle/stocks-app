2a0a7d0d6bb14784ee2c80a2504d0511e0fa39bf
Elle Abalos <elle.abalos@outlook.com>
2022-04-18 19:11:04 +0800

    updated helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def active_class(path)
+        request.path === path ? 'active' : 'text-white'
+    end
+
+    def custom_js
+        javascript_pack_tag "#{params[:controller]}/#{params[:action]}", 'data-turbolinks-track': 'reload'
+    rescue Webpacker::Manifest::MissingEntryError
+    end
 end
