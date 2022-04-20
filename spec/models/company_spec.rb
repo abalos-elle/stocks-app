1a510abb89c66a9a44a25b6f9cd2c7fb6a91fab6
ermzkie <ermawindelynlagustan@gmail.com>
2022-04-21 00:19:43 +0800

    Added tests for models

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -3,18 +3,44 @@ require 'rails_helper'
 RSpec.describe Company, type: :model do
     context 'validations' do
         let!(:company){Company.new}
-        it 'is a not valid company' do
-            company.name = "Erma Win, Inc."
+        it 'does not accept without a name' do
+            company.cik = 100
+            company.ticker = "EWL"
+            expect(company).to_not be_valid
+            expect(company.errors).to be_present
+            expect(company.errors.to_hash.keys).to include(:name)
+        end
+        it 'does not accept without a cik' do
+            company.name = "EWL, Inc."
+            company.ticker = "EWL"
             expect(company).to_not be_valid
             expect(company.errors).to be_present
             expect(company.errors.to_hash.keys).to include(:cik)
         end
         
-        it 'is a not valid company' do
-            company.name = "Erma Win, Inc."
+        it 'does not accept without a ticker' do
+            company.name = "EWL, Inc."
+            company.cik = 100
             expect(company).to_not be_valid
             expect(company.errors).to be_present
             expect(company.errors.to_hash.keys).to include(:ticker)
+        end
+
+        it 'does not accept an existing ticker' do
+            company.cik = 100
+            company.name = "EWL, Inc."
+            company.ticker = "EWL"
+
+            expect(company).to_not be_valid
+            expect(company.errors).to be_present
+            
+            company.save
+
+            another_company = Company.new
+            another_company.cik = 101
+            another_company.name = "Another EWL, Inc."
+            another_company.ticker = "EWL"
+            expect(another_company).to_not be_valid
         end
     end
 
