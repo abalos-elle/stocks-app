9110fbf2a3622c1be5f866efd134c0a4ee9e811b
Patricia Navarro <navarropatricia15@yahoo.com.ph>
2022-04-21 02:44:36 -0700

    added buttons to stock page

--- app/jobs/fetch_company_overview_job.rb
+++ app/jobs/fetch_company_overview_job.rb
@@ -6,7 +6,6 @@ class FetchCompanyOverviewJob < ApplicationJob
 
     os = OverviewService.new
     company.market_capitalization = os.fetch_company_overview(company.ticker).parsed_response["MarketCapitalization"]
-    #company.market_capitalization = os.fetch_market_capitalization(company.ticker).parsed_response["MarketCapitalization"]
     company.beta = os.fetch_company_overview(company.ticker).parsed_response["Beta"]
     company.pe_ratio = os.fetch_company_overview(company.ticker).parsed_response["PERatio"]
     company.eps = os.fetch_company_overview(company.ticker).parsed_response["EPS"]
--- app/models/overview_service.rb
+++ app/models/overview_service.rb
@@ -5,28 +5,8 @@ class OverviewService
 
     base_uri "www.alphavantage.co"
 
-    def fetch_market_capitalization(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=844bccca7f9d2d6179a2964c6d3cebca"
-        self.class.get(url)
-    end
-
-    def fetch_beta(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=844bccca7f9d2d6179a2964c6d3cebca"
-        self.class.get(url)
-    end
-
-    def fetch_pe_ratio(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=844bccca7f9d2d6179a2964c6d3cebca"
-        self.class.get(url)
-    end
-
-    def fetch_eps(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=844bccca7f9d2d6179a2964c6d3cebca"
-        self.class.get(url)
-    end
-
-    def fetch_ex_dividend_date(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=844bccca7f9d2d6179a2964c6d3cebca"
+    def fetch_company_overview(ticker)
+        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{Rails.application.credentials.dig(:alphavantage, :key)}"
         self.class.get(url)
     end
 end
