92c0ae0ebf4e4b80d8891f135b4cba573562e958
ermzkie <ermawindelynlagustan@gmail.com>
2022-04-25 22:58:26 +0800

    Set one api call for all details; removed individual calls

--- app/api/overview_service.rb
+++ app/api/overview_service.rb
@@ -5,28 +5,10 @@ class OverviewService
 
     base_uri "www.alphavantage.co"
 
-    def fetch_market_capitalization(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{Rails.application.credentials.dig(:alphavantage, :key)}"
+    def fetch_company_overview(ticker)
+        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=
+        #{Rails.application.credentials.dig(:alphavantage, :key)}"
         self.class.get(url)
-    end
-
-    def fetch_beta(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{Rails.application.credentials.dig(:alphavantage, :key)}"
-        self.class.get(url)
-    end
-
-    def fetch_pe_ratio(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{Rails.application.credentials.dig(:alphavantage, :key)}"
-        self.class.get(url)
-    end
-
-    def fetch_eps(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{Rails.application.credentials.dig(:alphavantage, :key)}"
-        self.class.get(url)
-    end
-
-    def fetch_ex_dividend_date(ticker)
-        url = "/query?function=OVERVIEW&symbol=#{ticker}&apikey=#{Rails.application.credentials.dig(:alphavantage, :key)}"
-        self.class.get(url)
+    rescue Alphavantage::Error
     end
 end
