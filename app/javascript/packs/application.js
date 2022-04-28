c0e2b743cb963f559b78f8f2af4e120fd7b55fff
ermzkie <ermawindelynlagustan@gmail.com>
2022-04-28 22:08:02 +0800

    Removed rendering of Chart.js graph

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,14 +24,14 @@ window.addEventListener('turbolinks:load', () => {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })   
 
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: JSON.parse(ctx.canvas.dataset.labels),
-        datasets: [{
-        data: JSON.parse(ctx.canvas.dataset.data),
-        }]
-    },
-    });
+    // var ctx = document.getElementById('myChart').getContext('2d');
+    // var myChart = new Chart(ctx, {
+    // type: 'line',
+    // data: {
+    //     labels: JSON.parse(ctx.canvas.dataset.labels),
+    //     datasets: [{
+    //     data: JSON.parse(ctx.canvas.dataset.data),
+    //     }]
+    // },
+    // });
 })
